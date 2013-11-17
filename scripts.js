df0c195cd5980b2d49faa7cb6a63a9489e69bc0b
zinas <zinas.nikos@gmail.com>
2013-11-17 15:37:47 +0100

    minor styling changes

--- scripts.js
+++ scripts.js
@@ -5,10 +5,11 @@ xtag.registerResponsive('x-nature', {
     },
     queries: {
         'max(200)' : function () {
-            this.innerHTML = '<div class="tree tree-1"></div>';
+            this.innerHTML = '<h4>I am just a tree</h4><div class="tree tree-1"></div>';
         },
         'min(201) and max(400)' : function () {
             this.innerHTML =
+            '<h4>I am a forest!</h4>'+
             '<div class="forest">'+
                 '<div class="tree tree-1"></div>'+
                 '<div class="tree tree-2"></div>'+
@@ -18,6 +19,7 @@ xtag.registerResponsive('x-nature', {
         },
         'min(401)' : function () {
             this.innerHTML =
+            '<h4>I am a whole jungle!</h4>'+
             '<div class="jungle">'+
                 '<div class="tree tree-1"></div>'+
                 '<div class="tree tree-2"></div>'+
